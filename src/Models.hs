59102b70098b3714fdb2291a137eca9ce9cb02ff
Mateusz Nowotyński <maxmati@memleak.pl>
2017-03-31 19:30:28 +0200

    Compiling using gcc

--- src/Models.hs
+++ src/Models.hs
@@ -6,8 +6,24 @@ import Data.Int (Int64)
 import GHC.Generics
 import Data.Aeson
 
-newtype Task = Task {
-    id :: Int
+type Source = String
+
+type Program = FilePath
+
+data CompilerParam = Param String | OutputFile | SourceCode deriving(Show)
+
+data CompilerConfig = CompilerConfig {
+    compiler :: FilePath,
+    args :: [CompilerParam],
+    stdin :: CompilerParam
+} deriving(Show)
+
+data Language = C | CPP deriving (Show, Generic, FromJSON)
+
+data Task = Task {
+    id :: Int,
+    source :: Source,
+    language :: Language
 } deriving (Show, Generic, FromJSON)
 
 newtype TaskResult = TaskResult {
--- src/Worker.hs
+++ src/Worker.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Worker where
 
 import ZeroMQ
 import Json
 import Models
+import Compiler
+
+import System.IO.Temp
 
 startWorker :: IO ()
-startWorker = start0MQWorker $ parseTask (\x -> TaskResult "Szatan czyste zło")
+startWorker = start0MQWorker $ parseTask processTask
 
+processTask :: Task -> IO TaskResult
+processTask task = withSystemTempDirectory "zmora-judge" $ \directory -> do
+    program <- compile task directory
+    let test = fromRight program
+    return $ TaskResult test
+
+fromRight (Right a) = a
