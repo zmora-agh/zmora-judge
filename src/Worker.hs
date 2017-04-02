a00b1d8545095f5bcd5aca3ccd33c77b882e41be
Mateusz Nowotyński <maxmati@memleak.pl>
2017-04-03 00:07:22 +0200

    New compiler syntax

--- src/Worker.hs
+++ src/Worker.hs
@@ -6,6 +6,7 @@ import ZeroMQ
 import Json
 import Models
 import Compiler
+import System.Directory
 
 import System.IO.Temp
 
@@ -14,8 +15,22 @@ startWorker = start0MQWorker $ parseTask processTask
 
 processTask :: Task -> IO TaskResult
 processTask task = withSystemTempDirectory "zmora-judge" $ \directory -> do
-    program <- compile task directory
-    let test = fromRight program
-    return $ TaskResult test
+    setCurrentDirectory directory
+    exampleProblemJudge $ source task
+
 
 fromRight (Right a) = a
+
+
+
+exampleProblemJudge :: Source -> IO TaskResult
+exampleProblemJudge input = do
+--     save input "source.c"
+
+    result <- withCompiler (defaultPreset :: GCC) $ do
+        blacklist "/usr/include/X11"
+        compile "source.c" "a.out"
+
+    return $ case result of
+        CompilationOK -> TaskResult "Success"
+        CompilationError error -> TaskResult error
