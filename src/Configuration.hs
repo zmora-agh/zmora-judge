9b44a5adad5afa61be0b974306c672987a6b501f
Mateusz Nowotyński <maxmati@memleak.pl>
2017-05-08 14:18:52 +0200

    Change nsjail path

--- src/Configuration.hs
+++ src/Configuration.hs
@@ -3,6 +3,6 @@ module Configuration where
 import Network.AMQP
 
 gccPath = "/usr/bin/gcc"
-nsJailPath = "/home/maxmati/src/foss/nsjail/nsjail" --TODO: use path from zmora-distro
+nsJailPath = "/usr/bin/nsjail"
 
 rabbitMQConnectionOpts = defaultConnectionOpts
