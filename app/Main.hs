6587240781ee010cb10befec8d33ed4129f93ebc
Mateusz Nowotyński <maxmati4@gmail.com>
2017-07-24 19:31:48 +0200

    Verify existance of gcc and nsjail on start (#20)

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import           Configuration               (validateConfiguration)
 import           Control.Monad.IO.Class
 import           Control.Monad.Logger.Syslog (runSyslogLoggingT)
 import           System.Environment          (getArgs)
@@ -8,6 +9,7 @@ import           Worker
 main :: IO ()
 main = do
   runSyslogLoggingT $ do
+    validateConfiguration
     args <- liftIO $ getArgs
     case args of
       (brokerURI:_) -> startWorker (Just brokerURI)
--- src/Configuration.hs
+++ src/Configuration.hs
@@ -1,8 +1,49 @@
-module Configuration where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+module Configuration
+   ( gccPath,
+     nsJailPath,
+     rabbitMQConnectionOpts,
+     validateConfiguration
+   ) where
 
-import Network.AMQP
+import           Control.Monad.IO.Class
+import           Control.Monad.Logger
+import           Data.Monoid            ((<>))
+import qualified Data.Text              as T
+import           Network.AMQP
+import           System.Exit
+import           System.IO.Error
+import           System.Process         (readProcessWithExitCode)
 
-gccPath = "/usr/bin/gcc"
-nsJailPath = "/usr/bin/nsjail"
+gccPath :: [Char]
+gccPath = "gcc"
+nsJailPath :: [Char]
+nsJailPath = "nsjail"
 
+rabbitMQConnectionOpts :: ConnectionOpts
 rabbitMQConnectionOpts = defaultConnectionOpts
+
+validateConfiguration :: (MonadLoggerIO m) => m ()
+validateConfiguration = do
+  validateGCC
+  validateNsJail
+
+validateExecutable :: MonadLoggerIO m => FilePath -> [String] -> m ()
+validateExecutable path params = do
+  result <- liftIO $ tryIOError $ readProcessWithExitCode path params ""
+  verifyExecution (T.pack path) result
+
+validateNsJail :: (MonadLoggerIO m ) => m ()
+validateNsJail = validateExecutable nsJailPath ["-h"]
+
+validateGCC :: (MonadLoggerIO m ) => m ()
+validateGCC = validateExecutable gccPath ["--version"]
+
+verifyExecution :: (MonadLoggerIO m ) => T.Text -> (Either IOError (ExitCode, String, String)) -> m ()
+verifyExecution path (Right (_, stdout, stderr)) =
+  $(logInfo) $ "Discovered " <> path <> ". Stdout:\n" <> T.pack stdout <>
+    "\nStderr:\n" <> T.pack stderr
+verifyExecution path (Left err) = do
+  $(logError) $ "Failed to run " <> path <> ".\n Error:" <> (T.pack . show) err
+  liftIO $ exitFailure
